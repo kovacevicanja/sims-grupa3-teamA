3f6f1b7a7091b470e6a0011534ed058d44b569a0
Anja Kovacevic <anjakovacevic9455@gmail.com>
2023-03-07 20:50:06 +0100

    initial project

---
+++ Controller/UserController.cs
@@ -0,0 +1,31 @@
+﻿using BookingProject.Model;
+using BookingProject.Serializer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingProject.Controller
+{
+    public class UserController
+    {
+        private const string FilePath = "../../Resources/Data/users.csv";
+
+        private readonly Serializer<User> _serializer;
+
+        private List<User> _users;
+
+        public UserController()
+        {
+            _serializer = new Serializer<User>();
+            _users = _serializer.FromCSV(FilePath);
+        }
+
+        public User GetByUsername(string username)
+        {
+            _users = _serializer.FromCSV(FilePath);
+            return _users.FirstOrDefault(u => u.Username == username);
+        }
+    }
+}
---
+++ View/SignInForm.xaml.cs
@@ -0,0 +1,96 @@
+﻿using BookingProject.Controller;
+using BookingProject.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BookingProject.View
+{
+    /// <summary>
+    /// Interaction logic for SignInForm.xaml
+    /// </summary>
+    public partial class SignInForm : Window
+    {
+
+        private readonly UserController _controller;
+
+        public bool IsSelectedOwner { get; set; }
+        public bool IsSelectedGuest1 { get; set; }
+        public bool IsSelectedGuest2 { get; set; }
+        public bool IsSelectedGuide { get; set; }
+
+        private string _username;
+        public string Username
+        {
+            get => _username;
+            set
+            {
+                if (value != _username)
+                {
+                    _username = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public SignInForm()
+        {
+            InitializeComponent();
+            DataContext = this;
+            _controller = new UserController();
+        }
+
+        private void SignIn(object sender, RoutedEventArgs e)
+        {
+            User user = _controller.GetByUsername(Username);
+            if (user != null)
+            {
+                if (user.Password == txtPassword.Password)
+                {
+                    if (IsSelectedOwner)
+                    {
+                        MessageBox.Show("Ulogovan vlasnik!");
+                    }
+                    else if(IsSelectedGuest1){
+                        MessageBox.Show("Ulogovan gost1!");
+                    }else if(IsSelectedGuest2)
+                    {
+                        MessageBox.Show("Ulogovan gost2!");
+                    }else if (IsSelectedGuide)
+                    {
+                        MessageBox.Show("Ulogovan vodic!");
+                    }
+                    Close();
+                }
+                else
+                {
+                    MessageBox.Show("Wrong password!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Wrong username!");
+            }
+
+        }
+    }
+}
