783f3dc41a4e4ee7205481b044f09a59bb0b33f7
Matija Maksimović <116630740+MatijaMax@users.noreply.github.com>
2023-04-04 00:55:59 +0200

    defining guide-user

---
+++ View/GuideView/TourCancellationWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BookingProject.View.GuideView
+{
+    /// <summary>
+    /// Interaction logic for TourCancellationWindow.xaml
+    /// </summary>
+    public partial class TourCancellationWindow : Window
+    {
+        public TourCancellationWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
