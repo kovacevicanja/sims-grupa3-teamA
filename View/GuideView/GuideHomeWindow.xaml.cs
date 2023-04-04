2e6b99e6c31a510bb38f4f63daba1470b1374229
Matija Maksimović <116630740+MatijaMax@users.noreply.github.com>
2023-04-04 16:09:37 +0200

    guide home window added

---
+++ View/GuideView/GuideHomeWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BookingProject.View.GuideView
+{
+    /// <summary>
+    /// Interaction logic for GuideHomeWindow.xaml
+    /// </summary>
+    public partial class GuideHomeWindow : Window
+    {
+        public GuideHomeWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
