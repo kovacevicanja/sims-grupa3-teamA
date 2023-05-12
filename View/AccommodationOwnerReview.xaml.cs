284448e8b6199f5debdcaacb71e5cdbd57e14e62
Matija Maksimović <116630740+MatijaMax@users.noreply.github.com>
2023-05-12 20:01:01 +0200

    packaging fix

---
+++ View/AccommodationOwnerReview.xaml.cs
@@ -0,0 +1,35 @@
+﻿using BookingProject.Controller;
+using BookingProject.Controllers;
+using BookingProject.DependencyInjection;
+using BookingProject.Domain.Images;
+using BookingProject.Model;
+using BookingProject.Model.Images;
+using BookingProject.Repositories.Intefaces;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BookingProject.View
+{
+    public partial class AccommodationOwnerReview : Window
+    {
+        public AccommodationOwnerReview(AccommodationReservation selectedReservation)
+        {
+            InitializeComponent();
+            this.DataContext = new AccommodationOwnerReviewViewModel(selectedReservation);
+        }
+    }
+}
