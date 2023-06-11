093a159a3c32214b66001119b3472baad0abcc4d
petrovicmilica <116906072+petrovicmilica@users.noreply.github.com>
2023-06-11 12:20:32 +0200

    show all forums

--- Domain/Forum.cs
+++ Domain/Forum.cs
@@ -1,4 +1,4 @@
-﻿using BookingProject.Model;
+﻿ using BookingProject.Model;
 using BookingProject.Serializer;
 using System;
 using System.Collections.Generic;
@@ -15,6 +15,8 @@ namespace BookingProject.Domain
         public Location Location { get; set; }
         public List<ForumComment> Comments { get; set; }
         public bool IsUseful { get; set; }
+        public string Status { get; set; }
+        public User User { get; set; }
 
         public Forum() {
             Location = new Location();
@@ -27,6 +29,8 @@ namespace BookingProject.Domain
             Name = values[1];
             Location.Id = int.Parse(values[2]);
             IsUseful = bool.Parse(values[3]);
+            Status = values[4];
+            User.Id = int.Parse(values[5]);
         }
 
         public string[] ToCSV()
@@ -36,7 +40,9 @@ namespace BookingProject.Domain
                 Id.ToString(),
                 Name,
                 Location.Id.ToString(),
-                IsUseful.ToString()
+                IsUseful.ToString(),
+                Status.ToString(),
+                User.Id.ToString()
             };
             return csvValues;
         }
